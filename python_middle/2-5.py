6cad192aadef84ef97e75775c23ab55a9241f38f
LouisWu <wuhaow@cn.ibm.com>
2019-12-17 10:26:04 +0800

    Add lambda:

--- python_middle/2-5.py
+++ python_middle/2-5.py
@@ -33,7 +33,9 @@ print(key_list)
 dict_list = [dict1, dict2, dict3]  # 以下编程假设列表长度未知
 start = time.time()
 
-key_list = reduce(lambda a, b: a & b, map(lambda dic: dic.keys(), dict_list))
+# key_list = reduce(lambda a, b: a & b, map(lambda dic: dic.keys(), dict_list))
+key_list = reduce(lambda a, b: a & b, map(dict.keys(), dict_list))  # 上面这行可以简化，用内置函数代替自定义的lambda函数
+
 end = time.time()
 print((end - start) * 1000)
 print(key_list)
@@ -83,3 +85,34 @@ print(key_list)
 # True
 # all(())
 # True
+
+# lambda函数
+# 匿名函数lambda的用法
+#
+# Python中，lambda函数也叫匿名函数，及即没有具体名称的函数，它允许快速定义单行函数，类似于C语言的宏，可以用在任何需要函数的地方。这区别于def定义的函数。
+# lambda与def的区别：
+# 1）def创建的方法是有名称的，而lambda没有。
+# 2）lambda会返回一个函数对象，但这个对象不会赋给一个标识符，而def则会把函数对象赋值给一个变量（函数名）。
+# 3）lambda只是一个表达式，而def则是一个语句。
+# 4）lambda表达式” : “后面，只能有一个表达式，def则可以有多个。
+# 5）像if或for或print等语句不能用于lambda中，def可以。
+# 6）lambda一般用来定义简单的函数，而def可以定义复杂的函数。
+# 6）lambda函数不能共享给别的程序调用，def可以。
+# lambda语法格式：
+# lambda 变量 : 要执行的语句
+# 冒号前面是参数，可以有多个，用逗号分隔，冒号右边是返回值。
+# 使用例子：
+#
+# g = lambda x: x * x   # 冒号左边是入参，后面的表达式是输出
+# g(3)
+# 9
+#
+# g = lambda x, y, z : (x + y) ** z    # 冒号左边的入参可以是多个
+# g(1,2,2)
+# 9
+#
+# list_a = [lambda a: a**3, lambda b: b**3]   # lambda可以嵌套在里面
+# list_a[0]
+# <function <lambda> at 0x106e58e18>
+# list_a[0](3)
+# 27
